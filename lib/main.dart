2977824032f6e358e99033e83b4080b4d5c3b7cc
hashemsh <sh.sheikhypour@gmail.com>
2021-12-23 17:41:31 +0330

    make AppBar and first Column and Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,10 +27,62 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello world !'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Curriculum Vitae'),
+          actions: const [
+            Icon(CupertinoIcons.bubble_left),
+            Padding(
+              padding: EdgeInsets.fromLTRB(8, 0, 16, 0),
+              child: Icon(CupertinoIcons.sun_min),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'assets/images/profile_image.png',
+                      width: 60,
+                      height: 60,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Brice Seraphin'),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      const Text('Product & Print Designer'),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Row(
+                        children: const [
+                          Icon(CupertinoIcons.location),
+                          Text('Paris, France')
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 32, 16),
+              child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea '),
+            )
+          ],
+        ));
   }
 }
