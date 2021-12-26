cf77e4629b155260cff7926c2000c2401bce594a
hashem sheikhypour <sh.sheikhypour@gmail.com>
2021-12-26 19:02:52 +0330

    change title app

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UserProfile',
       theme: themeMode == ThemeMode.dark
           ? MyAppThemeConfig.dark().getTheme()
           : MyAppThemeConfig.light().getTheme(),
